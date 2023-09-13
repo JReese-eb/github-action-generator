15dd4ab418fd197c766a04a0bb3b615b4c0468e4
Jesse Reese <jesse.reese@employbridge.com>
2023-09-13 14:09:34 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'github-action-generator';
+  githubActionForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.githubActionForm = this.fb.group({
+      jobName: ['', Validators.required],
+      runsOn: ['ubuntu-latest', Validators.required]
+    });
+  }
+
+  generateYAML() {
+    const formValue = this.githubActionForm.value;
+
+    const yamlContent = `
+      name: Generated Workflow
+      on: push
+      jobs:
+        ${formValue.jobName}:
+          runs-on: ${formValue.runsOn}
+          steps:
+          - name: Checkout code
+            uses: actions/checkout@v2
+    `;
+
+    console.log(yamlContent);
+    // You can then display this YAML in a text area or download it as a file
+  }
+
 }
