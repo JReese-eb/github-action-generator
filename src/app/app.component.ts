7494478b057bba010e0b1b48d15307db7a6d6b49
Jesse Reese <jesse.reese@employbridge.com>
2023-09-14 12:09:00 -0700

    Added basic styles for the UI, fixed form errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray, AbstractControl, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -11,23 +11,82 @@ export class AppComponent {
 
   constructor(private fb: FormBuilder) {
     this.githubActionForm = this.fb.group({
-      jobName: ['', Validators.required],
-      runsOn: ['ubuntu-latest', Validators.required]
+      workflowName: ['', Validators.required],
+      on: ['push', Validators.required],
+      jobName: ['boss-deploy', Validators.required],
+      runsOn: ['ubuntu-latest', Validators.required],
+      environmentVariables: this.fb.array([
+        this.fb.group({
+          label: [''],
+          value: ['']
+        })
+      ]),
+      steps: this.fb.array([
+        this.fb.group({
+          stepName: ['', Validators.required],
+          usesAction: [''],
+          runCommand: ['']
+        })
+      ])
     });
   }
 
+  get environmentVariables() {
+    return (this.githubActionForm.get('environmentVariables') as FormArray);
+  }
+
+  get steps() {
+    return (this.githubActionForm.get('steps') as FormArray);
+  }
+
+  addEnvironmentVariable() {
+    this.environmentVariables.push(this.fb.group({
+      label: [''],
+      value: ['']
+    }));
+  }
+
+  addStep() {
+    const step = this.fb.group({
+      stepName: ['', Validators.required],
+      usesAction: [''],
+      runCommand: ['']
+    });
+
+    if (step instanceof FormGroup) {
+      this.steps.push(step);
+    }
+  }
+
+
   generateYAML() {
     const formValue = this.githubActionForm.value;
+    let envYAML = '';
+    formValue.environmentVariables.forEach((env: any) => {
+      envYAML += ` ${env.label}: ${env.value}\n`;
+    });
+
+    let stepsYAML = '';
+    formValue.steps.forEach((step: any) => {
+      stepsYAML += `- name: ${step.stepName}\n`;
+      if (step.usesAction) {
+        stepsYAML += ` uses: ${step.usesAction}\n`;
+      }
+      if (step.runCommand) {
+        stepsYAML += ` run: ${step.runCommand}\n`;
+      }
+    });
 
     const yamlContent = `
-      name: Generated Workflow
-      on: push
+      name: ${formValue.workflowName}
+      on: ${formValue.on}
       jobs:
         ${formValue.jobName}:
           runs-on: ${formValue.runsOn}
+          env:
+            ${envYAML}
           steps:
-          - name: Checkout code
-            uses: actions/checkout@v2
+            ${stepsYAML}
     `;
 
     console.log(yamlContent);
